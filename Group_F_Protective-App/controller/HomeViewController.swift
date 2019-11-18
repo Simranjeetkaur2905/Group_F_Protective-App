1b51dc89444e372c5029bd568dbe3caa67fa3298
simranjeetkaur <chakkalsimmi@gmail.com>
2019-11-18 17:33:56 -0500

    changes in home page

--- Group_F_Protective-App/controller/HomeViewController.swift
+++ Group_F_Protective-App/controller/HomeViewController.swift
@@ -8,27 +8,9 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class HomeViewController: UIViewController {
 
     
-    var imgArr = ["Photos", "Password" ,  "Notes" , "Vedio", "Camera","Wallet","Recording"]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imgArr.count
-    }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as?
-             // cell.carname = carname[indexPath.row]
-              cell?.image.image = UIImage(named: imgArr[indexPath.row])
-              cell?.labletext.text = imgArr[indexPath.row]
-              
-              return cell!
-    }
     }
 
