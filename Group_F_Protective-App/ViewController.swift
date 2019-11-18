45eeca19143752a72ec5bf034ba566b658b59734
simranjeetkaur <chakkalsimmi@gmail.com>
2019-11-17 22:24:37 -0500

    Initial Commit

---
+++ Group_F_Protective-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group_F_Protective-App
+//
+//  Created by Simran Chakkal on 2019-11-17.
+//  Copyright © 2019 Simran Chakkal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Group_F_Protective-AppTests/Group_F_Protective_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Group_F_Protective_AppTests.swift
+//  Group_F_Protective-AppTests
+//
+//  Created by Simran Chakkal on 2019-11-17.
+//  Copyright © 2019 Simran Chakkal. All rights reserved.
+//
+
+import XCTest
+@testable import Group_F_Protective_App
+
+class Group_F_Protective_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
